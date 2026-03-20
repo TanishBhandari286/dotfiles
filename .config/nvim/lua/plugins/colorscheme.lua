6866a0426a928b66e720832e5bd1b0e1617eb0c7
tanishbhandari286 <tanishbhandari91@gmail.com>
2026-03-20 21:29:30 +0530

    moved to lazyvim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "main",
+      disable_background = true,
+      styles = { transparency = true },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
